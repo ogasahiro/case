e02a4bb5d3e7503aac0f16134a1c3254bc245977
hiro <hiro.net.study@gmail.com>
2023-12-03 08:21:57 +0900

    message

--- src/app/Http/Controllers/WorkController.php
+++ src/app/Http/Controllers/WorkController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Work;
-use App\Models\Break;
+
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
@@ -35,51 +35,27 @@ class WorkController extends Controller
   
 }
 public function workend(Request $request){
+  
 
   $user = Auth::user();
   $work = Work::create([
        'user_id' => $user->id,
        'workend' => Carbon::now()->format('H:i'),
         ]);
+        $works = Work::all();
+       dd($works); 
       return redirect()->back();
 }
 public function breakstart(Request $request){
   
   $work = Auth::work();
-  $break = Break::create([
-       'work_id' => $work->id,
-       'breakstart' => Carbon::now()->format('H:i'),
-        ]);
-      return redirect()->back();
+  
 }
 public function breakend(Request $request){
   $work = Auth::work();
-  $break = Break::create([
-       'work_id' => $work->id,
-       'breakend' => Carbon::now()->format('H:i'),
-        ]);
-      return redirect()->back();
-}
-public function check(Request $request)
-{
-  $text = ['text' => 'ログインして下さい。'];
-  return view('auth', $text);
-}
-
-public function checkUser(Request $request)
-{
-  $email = $request->email;
-  $password = $request->password;
-  if (Auth::attempt(['email' => $email,
-    'password' => $password])) {
-    $text =   Auth::user()->name . 'さんがログインしました';
-  } else {
-    $text = 'ログインに失敗しました';
-  }
-  return view('auth', ['text' => $text]);
+  
 }
 }
-?>
 
 
 
